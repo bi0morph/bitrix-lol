9eafbd58e071ca8eebcc4aa1d4324ded62787f62
Максим Тищенко <max@expertit.kh.ua>
2017-04-18 21:08:54 +0300

    Add text

--- news/index.php
+++ news/index.php
@@ -1,7 +1,9 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новости");
-?><?$APPLICATION->IncludeComponent("bitrix:news", ".default", array(
+?>
+Rus1 was here
+<?$APPLICATION->IncludeComponent("bitrix:news", ".default", array(
 	"IBLOCK_TYPE" => "news",
 	"IBLOCK_ID" => "1",
 	"NEWS_COUNT" => "5",
